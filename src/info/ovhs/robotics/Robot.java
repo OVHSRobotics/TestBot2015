d26038ae61a5aeb0df934152e455d20b75907049
OVHSRobotics4619 <ovhsrobotics@gmail.com>
2016-01-14 20:42:56 -0800

    Programmed Shooter
    Made the two shooter motors spin to shoot with victor speed.

--- src/info/ovhs/robotics/Robot.java
+++ src/info/ovhs/robotics/Robot.java
@@ -5,14 +5,19 @@ import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Victor;
 
 public class Robot extends IterativeRobot {
 	
 	//two speed controllers are being used. one on each side
 	
-	//assigns a Jaguar speedcontroller to the left and right
+	//assigns a Jaguar speedcontroller to the left and right for driving
 	Jaguar leftMotor = new Jaguar(0);
 	Jaguar rightMotor = new Jaguar(1);
+	
+	//assigns a Victor speedcontroller for shooting
+	Victor leftShooter = new Victor(3);
+	Victor rightShooter = new Victor(2);
 	
 	//creates new joystick objects to control the robot with a tank drive
 	Joystick leftJoystick = new Joystick(0);
@@ -37,6 +42,16 @@ public class Robot extends IterativeRobot {
      */
     public void teleopPeriodic() {
         testBotDrive.tankDrive(leftJoystick, rightJoystick, true);
+        if (leftJoystick.getRawButton(1))
+        {
+        	leftShooter.set(1);
+        	rightShooter.set(-1);
+        }
+        else
+        {
+        	leftShooter.set(0);
+        	rightShooter.set(0);
+        }
     }
     
     /**
